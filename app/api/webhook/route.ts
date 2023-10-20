fec99163fbd961ae195f20ac4878497f41082b0d
giangutz <80666756+giangutz@users.noreply.github.com>
2023-10-20 17:16:40 +0800

    Update route.ts

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -51,11 +51,12 @@ export async function POST(req: Request) {
   }
  
   const eventType = evt.type;
-  console.log(eventType);
+
+  console.log({eventType})
  
   if(eventType === 'user.created') {
     const { id, email_addresses, image_url, username, first_name, last_name } = evt.data;
-    console.log(id, email_addresses, image_url, username, first_name, last_name);
+
     // Create a new user in your database
     const mongoUser = await createUser({
       clerkId: id,
@@ -64,7 +65,7 @@ export async function POST(req: Request) {
       email: email_addresses[0].email_address,
       picture: image_url,
     })
-    console.log(mongoUser);
+
     return NextResponse.json({ message: 'OK', user: mongoUser})
   }
   
