8e01b8b151220da8015648964244e7e00f0e6d0d
giangutz <80666756+giangutz@users.noreply.github.com>
2023-10-20 20:13:09 +0800

    added console log to debug webhook error

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -30,7 +30,8 @@ export async function POST(req: Request) {
   // Get the body
   const payload = await req.json()
   const body = JSON.stringify(payload);
- 
+  console.log(body);
+  
   // Create a new SVIX instance with your secret.
   const wh = new Webhook(WEBHOOK_SECRET);
  
