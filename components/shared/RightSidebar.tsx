77986ff4143fd9525dd45801f11182e344b6a7b4
giangutz <80666756+giangutz@users.noreply.github.com>
2023-11-25 19:10:23 +0800

    Update RightSidebar.tsx

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -24,7 +24,7 @@ const popularTags = [
 const RightSidebar = async () => {
   const hotQuestions = await getHotQuestions();
   const popularTags = await getTopPopularTags();
-  
+
   return (
     <section className="background-light900_dark200 light-border custom-scrollbar sticky right-0 top-0 flex h-screen w-[350px] flex-col overflow-y-auto border-l p-6 pt-36 shadow-light-300 dark:shadow-none max-xl:hidden">
       <div>
@@ -56,7 +56,7 @@ const RightSidebar = async () => {
                 key={tag._id}
                 _id={tag._id}
                 name={tag.name}
-                totalQuestions={tag.totalQuestions}
+                totalQuestions={tag.numberOfQuestions}
                 showCount
               />
             ))}
